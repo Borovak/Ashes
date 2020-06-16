d794ba95a7add1c9783aee2e2556efc72d8c5d91
Claude Brabant <brabantclaude@gmail.com>
2020-06-15 22:07:57 -0400

    Pickups use same script and states are saved

--- Assets/Scripts/PickupController.cs
+++ Assets/Scripts/PickupController.cs
@@ -4,21 +4,47 @@ using UnityEngine;
 
 public class PickupController : MonoBehaviour
 {
+    public enum Pickups
+    {
+        DoubleJump
+    }
+
+    public Pickups pickup;
+
+    private bool _pickupState
+    {
+        get => new Dictionary<Pickups, bool>
+        {
+            {Pickups.DoubleJump, PlayerPlatformerController.Instance.hasDoubleJump}
+        }[pickup];
+        set
+        {
+        new Dictionary<Pickups, System.Action<bool>>
+        {
+            {Pickups.DoubleJump, value => PlayerPlatformerController.Instance.hasDoubleJump = value}
+        }[pickup].Invoke(value);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (_pickupState)
+        {
+            GameObject.Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void OnTriggerEnter2D(Collider2D col){
+    void OnTriggerEnter2D(Collider2D col)
+    {
         Debug.Log("Pickup touched");
-        PlayerPlatformerController.Instance.hasDoubleJump = true;
-        GameObject.Destroy(gameObject);        
+        _pickupState = true;
+        GameObject.Destroy(gameObject);
     }
 }
