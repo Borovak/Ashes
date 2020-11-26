9d26726071e60c809a02b0336646549fd08613e3
Claude Brabant <brabantclaude@gmail.com>
2020-11-26 13:43:19 -0500

    Missing commandlinebuild file added

--- Assets/CommonFunctions/Editor/CommandLineBuild.cs
+++ Assets/CommonFunctions/Editor/CommandLineBuild.cs
@@ -8,6 +8,6 @@ public static class CommandLineBuild
     public static void BuildPlayer()
     {
         string[] scenes = { "Assets/Scenes/MainMenu.unity", "Assets/Scenes/LevelDesignerLoader.unity" };
-        BuildPipeline.BuildPlayer(scenes, @"C:\Users\Claude\Jenkins\workspace\Ashes\Build\Ashes.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+        BuildPipeline.BuildPlayer(scenes, "C:/Users/Claude/Jenkins/workspace/Ashes/Build/", BuildTarget.StandaloneWindows64, BuildOptions.None);
     }    
 }
