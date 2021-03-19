b8f303d3a291507a4f0eac3a3352ce1557f0bf61
Claude Brabant <brabantclaude@gmail.com>
2021-03-19 14:01:50 -0400

    scenes list update in build command

--- Assets/CommonFunctions/Editor/CommandLineBuild.cs
+++ Assets/CommonFunctions/Editor/CommandLineBuild.cs
@@ -6,7 +6,7 @@ namespace CommonFunctions.Editor
     {
         public static void BuildPlayer()
         {
-            string[] scenes = { "Assets/Scenes/MainMenu.unity", "Assets/Scenes/LevelDesignerLoader.unity" };
+            string[] scenes = { "Assets/Scenes/MainMenu.unity", "Assets/Scenes/Game.unity" };
             BuildPipeline.BuildPlayer(scenes, "C:/Users/Public/Documents/AshesBuild/Ashes.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
         }    
     }
